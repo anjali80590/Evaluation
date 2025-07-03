8318516a56a0fc7698b4e13331ec0ddfd9fd064c
Anjali <112229496+anjali80590@users.noreply.github.com>
2025-07-03 14:53:41 +0530

    improve ui

--- Evaluation3July/reatjsevaluation/src/App.js
+++ Evaluation3July/reatjsevaluation/src/App.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import {TaskProvider} from './context/TaskContext'
-import { Route, Router, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Tasks from './pages/Tasks'
-import About from './pages/About'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { TaskProvider } from "./context/TaskContext";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Tasks from "./pages/Tasks";
+import About from "./pages/About";
+
 function App() {
   return (
-    <div>
-      <TaskProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </TaskProvider>
-    </div>
+    <TaskProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </TaskProvider>
   );
 }
 
-export default App
+export default App;
--- Evaluation3July/reatjsevaluation/src/components/Navbar.jsx
+++ Evaluation3July/reatjsevaluation/src/components/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <div style={{padding:'10px',display:'flex',gap:'10px'}}>
-        <Link to='/'>Home</Link>
-        <Link to='/tasks'>Tasks</Link>
-        <Link to='/about'>About</Link>
+    <div
+      style={{
+        display: "flex",
+        gap: "20px",
+        padding: "10px",
+        background: "pink",
+      }}
+    >
+      <Link to="/">Home</Link>
+      <Link to="/tasks">Tasks</Link>
+      <Link to="/about">About</Link>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
